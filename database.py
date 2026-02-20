c425831f1001d99baa2ef39a5c03d9fdfc287281
Chaitanya <chaithanyavamsinaidu7@gmail.com>
2026-02-21 00:10:50 +0530

    Switched to SQLite for deployment

--- database.py
+++ database.py
@@ -2,9 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgres123@localhost:5434/agrisync"
+DATABASE_URL = "sqlite:///./agrisync.db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
